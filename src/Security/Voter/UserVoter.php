bd9162e1c156322b402dff5e7960e592b1382949
GabrieleCapocelli <93205404+GabrieleCapocelli@users.noreply.github.com>
2022-07-08 11:45:47 +0200

    splitted voters

--- src/Security/Voter/UserVoter.php
+++ src/Security/Voter/UserVoter.php
@@ -2,17 +2,16 @@
 
 namespace App\Security\Voter;
 
+use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Security;
-use App\Entity\User;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-
-class ReviewVoter extends Voter
+class UserVoter extends Voter
 {
-    public const REVIEW_EDIT = 'REVIEW_EDIT';
-    public const REVIEW_DELETE = 'REVIEW_DELETE';
+    public const USER_EDIT = 'USER_EDIT';
+    public const USER_DELETE = 'USER_DELETE';
     private Security $security;
 
     public function  __construct(Security $security)
@@ -23,14 +22,14 @@ class ReviewVoter extends Voter
     protected function supports(string $attribute, $subject): bool
     {
 
-        return in_array($attribute, [self::REVIEW_EDIT, self::REVIEW_DELETE, self::USER_EDIT, self::USER_DELETE])
-            && $subject instanceof \App\Entity\Review;
+        return in_array($attribute, [self::USER_EDIT, self::USER_DELETE])
+            && $subject instanceof \App\Entity\User;
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
-        // if the User is anonymous, do not grant access
+        // if the user is anonymous, do not grant access
         if (!$user instanceof UserInterface) {
             return false;
         }
@@ -38,29 +37,26 @@ class ReviewVoter extends Voter
         // check if User is admin
         if($this->security->isGranted('ROLE_ADMIN')) return true;
 
-        //check if review has an owner
-        if(false === $subject->getUser()) return false;
-
         // ... (check conditions and return true to grant permission) ...
         switch ($attribute) {
-            case self::REVIEW_EDIT:
-                return $this->canEditReview($subject, $user);
+            case self::USER_EDIT:
+                return $this->canEditUser($subject, $user);
                 break;
-            case self::REVIEW_DELETE:
-                return $this->canDeleteReview($subject, $user);
+            case self::USER_DELETE:
+                return $this->canDeleteUser($subject, $user);
                 break;
         }
 
         return false;
     }
 
-    protected function canDeleteReview(Review $review, User $user, ): bool
+    protected function canDeleteUser(User $account, User $user, ): bool
     {
-        return $user === $review->getUser();
+        return $user === $account->getUser();
     }
 
-    protected function canEditReview(Review $review, User $user): bool
+    protected function canEditUser(User $account, User $user): bool
     {
-        return $user === $review->getUser();
+        return $user === $account->getUser();
     }
 }
